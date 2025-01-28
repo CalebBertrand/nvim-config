cc5c6b3e56bfe0afacfbab5172932ce861ac7ecc
Caleb <developer.caleb@proton.me>
2025-01-28 17:43:26 -0600

    Tue Jan 28 05:43:26 PM CST 2025

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,5 +4,6 @@ vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden=true })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = 'Grep through current directory' })
 vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Search through references to current word' })
+vim.keymap.set('n', '<leader>gl', builtin.resume, { desc = 'Resume to last search' })
 
 
--- lua/caleb/packer.lua
+++ lua/caleb/packer.lua
@@ -1,46 +1,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} },
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} },
         opts = {
             pickers = {
                 lsp_references = {
                     theme = "dropdown"
                 },
-            }
+            },
         }
     }
 
-	use {
-		'tiagovla/tokyodark.nvim',
-		requires = { { 'tiagovla/tokyodark.nvim' } },
-		config = function() require('tokyodark').setup({ transparent_background = true }) end
-	}
+    use {
+        'tiagovla/tokyodark.nvim',
+        requires = { { 'tiagovla/tokyodark.nvim' } },
+        config = function() require('tokyodark').setup({ transparent_background = true }) end
+    }
 
     use { "ellisonleao/gruvbox.nvim" }
 
-	use {
-		'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }
-	}
+    use {
+        'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }
+    }
 
-	use('theprimeagen/harpoon')
+    use('theprimeagen/harpoon')
 
-	use('mbbill/undotree')
+    use('mbbill/undotree')
 
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
 
-	use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-	use('neovim/nvim-lspconfig')
-	use('hrsh7th/nvim-cmp')
-	use('hrsh7th/cmp-nvim-lsp')
+    use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+    use('neovim/nvim-lspconfig')
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
 
     use { 'norcalli/nvim-colorizer.lua' }
 end)
